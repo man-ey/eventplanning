310d89f9007aa7d8c594c3307f3b82123f594b8c
Stefan Lichtenstein <lichtenstein@fim.uni-passau.de>
2020-06-22 16:57:01 +0200

    Hinzufügen der basic Definitionen
    -Notwendigsten javax Importe
    
    -Definition der Klasse als Servlet

--- src/main/java/de/fim/wad/eventplanning/controller/DispatcherServlet.java
+++ src/main/java/de/fim/wad/eventplanning/controller/DispatcherServlet.java
@@ -3,3 +3,15 @@ Servlet responsible/handling the requests and assigning the fitting views
 and/or assigning fitting/necessary servlets.
 Starting up with (first) access to the (Home)Page
 */
+package de.fim.wad.eventplanning.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Servlet
+public class DispatcherServlet extends HttpServlet {
+
+}
