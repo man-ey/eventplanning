495ebee72c86101bc4c429368f8e852c848d6892
Stefan Lichtenstein <lichtenstein@fim.uni-passau.de>
2020-06-22 17:06:15 +0200

    Erstellen des (noch leeren) Servlets welches die Erstellung neuer Events handlet
    Beinhaltet die notwendigsten javax-Importe und Deklaration als Servlet

---
+++ src/main/java/de/fim/wad/eventplanning/controller/CreateNewEventServlet.java
@@ -0,0 +1,16 @@
+/*
+Servlet responsible for the creation of new events and their database entry
+*/
+
+package de.fim.wad.eventplanning.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Servlet
+public class CreateNewEventServlet extends HttpServlet {
+
+}
