74d244516448956f7083cf52273e7b036463bc55
Stefan Lichtenstein <lichtenstein@fim.uni-passau.de>
2020-06-23 09:40:12 +0200

    Adding (empty) Servlet for the Searching-Events Site

---
+++ src/main/java/de/fim/wad/eventplanning/controller/SearchingEventsServlet.java
@@ -0,0 +1,17 @@
+/*
+Servlet for the site displaying all events resulting in a search by
+name or location
+*/
+
+package de.fim.wad.eventplanning.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Servlet
+public class DispatcherServlet extends HttpServlet {
+
+}
