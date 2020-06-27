21c60ea355e1158029f987f2dfdd7e264dba3d02
Stefan Lichtenstein <lichte13@ads.uni-passau.de>
2020-06-27 21:57:33 +0200

    Beinahe fertigstellung der homepage + eventerstellung
    -Footer implementiert
    
    -dropdown menu zum filtern der events implementiert
    
    -füllung per thymeleaf implementiert aber noch nicht getestet -> noch zu testen/anzupassen
    
    -noch resolve errors deswegen
    
    -create event input geregelt über divs
    
    -noch kein feedback bei falschen inputs etc (backend initiiert?=
    
    -submit nur möglich wenn alles ausgefüllt
    
    -select box ob koord oder ortsname
    
    -übergabe der values per thymeleaf fields (noch zu testen)

---
+++ src/main/webapp/WEB-INF/javascript/CreateEventJS.js
@@ -0,0 +1,52 @@
+//start monitoring searchbar on keypress
+window.onkeyup = keyup;
+
+
+//Text in searchbar
+var searchBarInput;
+
+
+function keyup(e) {
+    //updating the string
+    searchBarInput = e.target.value;
+
+
+    //if loop starting when pressing enter
+    if (e.keyCode == 13) {
+        var destination = "?searchby=";
+        destination = destination.concat(searchBarInput);
+        reDirect(destination);
+    }
+}
+
+
+function reDirect(target) {
+    var domain = "http://localhost:8080/"
+    var destination = domain.concat(target);
+    location.replace(destination);
+}
+
+
+//Necessary javascriptline for the dropdownmenu
+$('.dropdown-toggle').dropdown()
+
+
+//Only enabling to submit if everything is filled out
+function checkInputs() {
+    var isValid = true;
+    $('input').filter('[required]').each(function() {
+        if ($(this).val() === '') {
+            $('#confirm').prop('disabled', true)
+            isValid = false;
+            return false;
+        }
+    });
+    if(isValid) {$('#confirm').prop('disabled', false)}
+    return isValid;
+}
+//Enable or disable button based on if inputs are filled or not
+$('input').filter('[required]').on('keyup',function() {
+    checkInputs()
+})
+
+checkInputs()
