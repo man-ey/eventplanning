4c7d4036c079b10b958d5b2f750f78da54f98a74
Stefan Lichtenstein <lichte13@ads.uni-passau.de>
2020-06-29 16:36:47 +0200

    FixUp HTML-strukutr + thymeleaf expressions

--- src/main/java/de/fim/wad/eventplanning/controller/IndexController.java
+++ src/main/java/de/fim/wad/eventplanning/controller/IndexController.java
@@ -1,30 +1,31 @@
 package de.fim.wad.eventplanning.controller;
 
+import de.fim.wad.eventplanning.dto.EventDTO;
 import de.fim.wad.eventplanning.model.Event;
 import org.springframework.http.HttpRequest;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.ModelAndView;
 
 
 import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
 @Controller
 public class IndexController {
 
     @RequestMapping("/")
-    public String homepage(){
-        System.out.println("called homepage");
+    public String homepage(Model model){
+ //       EventController dto = new EventController();
+   //     model.addAttribute("topTwenty", dto.newestTwenty());
+  //      System.out.println("called homepage");
         return "index";
     }
 
     @RequestMapping("/createEvent")
     public String createNewEvent(Model model){
-        model.addAttribute("createdEvent", new Event());
+        model.addAttribute("createdEvent", new EventDTO());
         return "CreateEvent";
     }
 
