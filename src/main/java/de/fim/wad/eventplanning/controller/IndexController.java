e5558c782d66e93e043da731e813e7d03ed8263e
ArturAntin <antin.artur@gmail.com>
2020-06-29 12:20:02 +0200

    rewrite architecture

---
+++ src/main/java/de/fim/wad/eventplanning/controller/IndexController.java
@@ -0,0 +1,37 @@
+package de.fim.wad.eventplanning.controller;
+
+import org.springframework.http.HttpRequest;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+
+
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping("/")
+    public String homepage(){
+        System.out.println("called homepage");
+        return "index";
+    }
+
+    @RequestMapping("/createEvent")
+    public String createNewEvent(){
+        return "CreateEvent";
+    }
+
+    @RequestMapping("/topTwenty")
+    public String topTwenty(){
+        return "TopTwenty";
+    }
+
+    @RequestMapping("/newestEvents")
+    public String newestEvents(){
+        return "index";
+    }
+}
