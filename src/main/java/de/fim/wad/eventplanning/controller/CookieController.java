6537e6f055991225b82195e0a9a06242918a88df
Stefan Lichtenstein <lichte13@ads.uni-passau.de>
2020-06-30 19:11:13 +0200

    Cleanup des Ordners + Anpassung CSS Files + Erstellung des Cookie Controllers
    -Überflüssige Java Dateien entfernt
    
    -CSS angepasst bzgl footer und buttons für like/dislike

---
+++ src/main/java/de/fim/wad/eventplanning/controller/CookieController.java
@@ -0,0 +1,66 @@
+package de.fim.wad.eventplanning.controller;
+
+import org.springframework.boot.web.servlet.server.Session;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
+public class CookieController {
+
+    /**
+     * Cookie initialisieren bei erstem besuch mit der liste an gelikten events
+     * @return cookie string (leer)
+     */
+    @GetMapping("/")
+    public String setCookieLikes(HttpServletResponse response){
+        Cookie cookie = new Cookie("likes", "");
+        // 1 day
+        cookie.setMaxAge(1 * 24 * 60 * 60);
+        cookie.setSecure(true);
+        cookie.setHttpOnly(true);
+        // global cookie accessible every where
+        cookie.setPath("/");
+        //add cookie to response
+        response.addCookie(cookie);
+
+        return "CookieLikes initiated";
+    }
+
+    /**
+     * Cookie initialisieren bei erstem besuch mit der liste an gedisliten events
+     * @return cookie string (leer)
+     */
+    @GetMapping("/")
+    public String setCookieDislikes(HttpServletResponse response) {
+        Cookie cookie = new Cookie("dislikes", "");
+        // 1 day
+        cookie.setMaxAge(1 * 24 * 60 * 60);
+        cookie.setSecure(true);
+        cookie.setHttpOnly(true);
+        // global cookie accessible every where
+        cookie.setPath("/");
+        //add cookie to response
+        response.addCookie(cookie);
+
+        return "CookieDislikes initiated";
+    }
+
+
+    /**
+     * @GetMapping("/detail")
+     *     public String readAllCookies(HttpServletRequest request) {
+     *
+     *
+}
+     *
+             *@GetMapping("/detail")
+     *public String setCookie(HttpServletResponse response){
+        *
+        *}
+     */
+}
